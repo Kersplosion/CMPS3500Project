b0cb7bf3cddd07a12832451e41b764db772d5bfc
Tristan Bock <littlepoindexter@gmail.com>
2021-11-09 16:28:39 -0800

    Fixed set issues in colClean. Changed duplicate section of rowClean to specify if all entries in a row are duplicates.

--- DataClean.py
+++ DataClean.py
@@ -16,22 +16,22 @@ def rowClean(valueArray):
             if item == '': #if a row has a missing column entry
                 flag = True
         s = set(valueArray[row])
-        if len(s) != len(valueArray[row]): #if a row has a duplicate entry
+        if (len(s) == 1 and len(s) != len(valueArray[row])): #if a row has all duplicate entries.
             flag = True
         if flag:
             toDelete.append(row) #collect all rows flagged for deletion into an index list.
-    toDelete.sort(reverse = True)
+    toDelete.sort(reverse=True)
     for row in toDelete:
         valueArray.pop(row) #We pop rows that qualify from the bottom up so we avoid range errors.
 
     return valueArray
 
 def colClean(valueArray):
-    toDeleteSet = {}
+    toDeleteSet = set()
     for col in range(len(valueArray[0])):
         for row in valueArray:
             if not row[col].isnumeric():
-                toDeleteSet.append(col) #if a column contains a nonnumeric string, flag for deletion
+                toDeleteSet.add(col) #if a column contains a nonnumeric string, flag for deletion
 
     toDelete = sorted(toDeleteSet, reverse=True) #This is now a list in descending order
     for item in toDelete:
